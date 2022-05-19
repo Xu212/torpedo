7433a3b33c0be5e52658dc3f00a49de922db5635
DeltazorZz <vetka.adi@gmail.com>
2022-05-19 12:55:21 +0200

    Pálya generálás

--- torpedo/src/main/PalyaOsztaly.java
+++ torpedo/src/main/PalyaOsztaly.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package main;
 
-/**
- *
- * @author hallgato
- */
 public class PalyaOsztaly {
+    private int meret;
+    private String szin;
+    public static final String KEK = "\u001B[44m";
     
+    
+    public PalyaOsztaly() {
+        this(7, KEK);
+        
+    
+    }
+
+    public PalyaOsztaly(int meret, String szin) {
+        this.meret = meret;
+        this.szin = szin;
+    }
+    
+   
+    public String[] PalyaGeneralas(){
+    String[] palya = new String[meret];
+        for (int i = 0; i < palya.length; i++) {
+            palya[i] = KEK + "_";
+        }
+        return palya;
+    }
 }
--- torpedo/src/main/TorpedoProgram.java
+++ torpedo/src/main/TorpedoProgram.java
@@ -1,6 +1,12 @@
 package main;
 public class TorpedoProgram {
     public static void main(String[] args) {
+        PalyaOsztaly palyaoszt = new PalyaOsztaly();
+        
+        String[] palya = palyaoszt.PalyaGeneralas();
+        for (int i = 0; i < palya.length; i++) {
+            System.out.print(palya[i]+" ");
+        }
         
     }
     
