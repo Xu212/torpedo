d1cc8b7722c3fe5f465399b7ab272f0f7646c55b
Xu Jiyu <matexu97@gmail.com>
2022-05-19 13:23:35 +0200

    hajó generálása

--- torpedo/src/main/HajoOsztaly.java
+++ torpedo/src/main/HajoOsztaly.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main;
 
-/**
- *
- * @author matex
- */
 public class HajoOsztaly {
-    
+
+    private int hajomeret;
+
+    public HajoOsztaly() {
+        this(3);
+    }
+
+    public HajoOsztaly(int hajomeret) {
+        this.hajomeret = hajomeret;
+    }
+
+    public int[] hajogen() {
+        int[] hajo = new int[hajomeret];
+        int i = 1;
+        int elozo;
+        hajo[0] = (int) (Math.random() * 7);
+        while (i < hajo.length) {
+            int hajohelye = (int) (Math.random() * 7);
+            if (hajo[i - 1] + 1 == hajohelye || hajo[i - 1] - 1 == hajohelye) {
+                int hasonlo = 0;
+                for (int j = 0; j < hajo.length; j++) {
+                    if (hajo[j] == hajohelye) {
+                        hasonlo++;
+                    }
+                }
+                if(hasonlo<hajo.length-2){
+                    hajo[i] = hajohelye;
+                        i++;
+                }
+            }
+        }
+        return hajo;
+    }
 }
--- torpedo/src/main/TorpedoProgram.java
+++ torpedo/src/main/TorpedoProgram.java
@@ -1,7 +1,11 @@
 package main;
 public class TorpedoProgram {
     public static void main(String[] args) {
-        
+        HajoOsztaly hajopeldany = new HajoOsztaly();
+        int[] hajo = hajopeldany.hajogen();
+        for (int i = 0; i < hajo.length; i++) {
+            System.out.print(hajo[i]);
+        }
     }
     
 }
